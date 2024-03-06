21d13103644054e8b318e22efb61670f018e0e9b
CHSFN <christoferferreira28@gmail.com>
2024-03-06 10:56:01 -0300

    criando a pagina e listando os emprestimos

---
+++ Controllers/EmprestimoController.cs
@@ -0,0 +1,25 @@
+using System.Data.Entity.Core.Common.CommandTrees;
+using EmprLivros.Data;
+using EmprLivros.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmprLivros.Controllers
+{
+    public class EmprestimoController : Controller
+    {
+        readonly private ApplicationDbContext _db;
+
+        public EmprestimoController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {  
+
+            IEnumerable<EmprestimosModel> emprestimos = _db.Emprestimos;
+
+            return View(emprestimos);
+        }
+    }
+}
